1e4fe0816786a0000fa05bf7d7977bcb51d9d633
Pietro Bode <pietro@example.com>
2025-05-18 23:02:00 -0700

    Update config.yml

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/18/eNSP/","2025/05/17/Hello-World/"];function toRandomPost(){
+var posts=["2025/05/17/Hello-World/","2025/05/18/eNSP/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
